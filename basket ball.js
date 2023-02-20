9ea04d4ace97c9aa51f1b6c9fcbcf06e5e64a9d9
Siddhant097 <119877320+Siddhant097@users.noreply.github.com>
2023-02-20 15:48:04 +0530

    aafds

---
+++ basket ball.js
@@ -0,0 +1,35 @@
+let home = document.getElementById("az")
+let guest = document.getElementById("bz")
+
+let count = 0
+let couunt=0
+function add1(){
+count+=1
+home.textContent= count
+
+}
+function add2(){
+    count+=2
+    home.textContent= count
+    
+    }
+    function add3(){
+        count+=3
+        home.textContent= count
+        
+        }
+        function addd1(){
+couunt+=1
+guest.textContent= couunt
+
+}
+function addd2(){
+    couunt+=2
+    guest.textContent= couunt
+    
+    }
+    function addd3(){
+        couunt+=3
+        guest.textContent= couunt
+        
+        }
